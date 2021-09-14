b135e9144e6a4c89702b791be0d0bc87b21a4a05
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-09-14 16:34:50 -0300

    Continuation of the implementation of page orders, file routes and axios connection

--- front-end/src/Orders/index.tsx
+++ front-end/src/Orders/index.tsx
@@ -1,11 +1,27 @@
 
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeaders';
 import './styles.css';
+import { Product } from './types';
 
 function Orders() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+
+  useEffect(() => {
+
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   )
 }
